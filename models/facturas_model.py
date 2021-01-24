382ddb6b4389261980d8e3b3990a9e57e1465c28
Javier <javiermtdam@gmail.com>
2021-01-24 21:32:34 +0100

    Modelo funcional, vistas no acabadas v2

--- models/facturas_model.py
+++ models/facturas_model.py
@@ -12,13 +12,14 @@ class facturas_model(models.Model):
     fecha = fields.Date(string="Fecha", required=True, default=date.today())
     cliente = fields.Many2one("empresa.clientes_model", string="Cliente")
     detallef = fields.One2many("empresa.detfac_model","facturas_id", string="Factura")
-    base = fields.Float(string="Base", compute="_calc_base")
+    base = fields.Float(string="Base", compute="_calc_base", store=True)
     iva = fields.Selection(string="IVA", default='21', selection=[('21','21'),('15', '15'),('7', '7'),('0', '0')], required=True)
-    total = fields.Float(string="Total", compute="_calc_iva")
+    total = fields.Float(string="Total", compute="_calc_iva", store=True)
     
 
     @api.depends('detallef')
     def _calc_base(self):
+        self.ensure_one()
         suma = 0
         for i in self.detallef:
             suma += i.productos_id.pvp*i.cantidad
@@ -26,6 +27,7 @@ class facturas_model(models.Model):
 
     @api.depends('iva', 'base')
     def _calc_iva(self):
+        self.ensure_one()
         self.total = (((self.base*int(self.iva))/100)+self.base)
 
     
